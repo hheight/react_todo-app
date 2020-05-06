d5c3f8d272328729d2009131476a1f7597301846
Vitalii Shytikov <v.shytikov@gmail.com>
2020-05-06 22:57:24 +0300

    Updates

---
+++ precache-manifest.029aa533a63f323aac98877afdc70f26.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05ea70993188b34abd8bf0619f674f13",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ab8a248a5e8fcbb9571c",
+    "url": "./static/css/main.e56c2ea1.chunk.css"
+  },
+  {
+    "revision": "88334e073198e96056f8",
+    "url": "./static/js/2.d0503e06.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "./static/js/2.d0503e06.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab8a248a5e8fcbb9571c",
+    "url": "./static/js/main.4f3a2121.chunk.js"
+  },
+  {
+    "revision": "548aac5e4720dc7e1ea8",
+    "url": "./static/js/runtime-main.9c94e0be.js"
+  }
+]);
